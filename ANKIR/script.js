3668b8bf83614c6311432f92dfbc739bf2b83295
Kirthana B <68461326+Kirthana5@users.noreply.github.com>
2025-07-06 20:10:02 +0530

    Delete ANKIR directory

--- ANKIR/script.js
+++
@@ -1,8 +0,0 @@
-const hands = document.getElementById('hands');
-const ring = document.getElementById('ring');
-const message = document.getElementById('message');
-
-hands.addEventListener('click', () => {
-  ring.style.display = 'block';
-  message.style.display = 'block';
-});
